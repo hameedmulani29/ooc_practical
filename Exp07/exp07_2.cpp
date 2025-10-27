d66b23822707de170a6352c09c5501dd9fedb54b
Hameed Mulani <hameedmulani15@gmail.com>
2025-10-27 12:51:07 +0530

    Added Experiment 7_2 to folder

--- Exp07/exp07_2.cpp
+++ Exp07/exp07_2.cpp
@@ -3,5 +3,33 @@ using namespace std;
 
 class Person{
     public:
-        
+        Person(){
+            cout << "Person's constructor called";
+        }
+};
+
+class Faculity : public Person{
+    public:
+        Faculity() {
+            cout << "Faculity's constructor called" << endl;
+        }
+};
+
+class Student : public Person{
+    public:
+        Student(){
+            cout << "Student's constructor called" << endl;
+        }
+};
+
+class TA : public Faculity , public Student{
+    public:
+        TA(){
+            cout << "TA's constructor called" <<  endl;
+        }
+};
+
+int main(){
+    TA ta;
+    return 0;
 }
